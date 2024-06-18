79a858bbb5009d27430ca42fd22541905e80f4c1
Jivesh Kumar <jivesh.kumar@technoforte.co.in>
2024-06-18 11:59:51 +0530

    version 2

--- src/main/java/com/jivesh/journal/controller/JournalEntryController.java
+++ src/main/java/com/jivesh/journal/controller/JournalEntryController.java
@@ -5,28 +5,48 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.jivesh.journal.entity.JournalEntry;
 
 @RestController
 @RequestMapping("/crud")
 public class JournalEntryController {
-	
-	private Map<Long, JournalEntry> journalEntries= new HashMap<>();
-	
-	@GetMapping()
-	public List<JournalEntry> getAll(){
-		return new ArrayList<>(journalEntries.values());
-		}
- @PutMapping()
-	public boolean createEntry(@RequestBody JournalEntry myEntry) {
-		journalEntries.put(myEntry.getId(),myEntry);
-		return true;
-	}
-	
+
+    private Map<Long, JournalEntry> journalEntries = new HashMap<>();
+
+    @GetMapping()
+    public List<JournalEntry> getAll() {
+        return new ArrayList<>(journalEntries.values());
+    }
+
+    @PostMapping()
+    public boolean createEntry(@RequestBody JournalEntry myEntry) {
+        journalEntries.put(myEntry.getId(), myEntry);
+        return true;
+    }
+
+    @GetMapping("id/{myId}")
+    public JournalEntry getJouranalEntryById(@PathVariable Long myId) {
+        return journalEntries.get(myId);
+    }
+
+    @DeleteMapping("id/{myId}")
+    public JournalEntry deleteJouranalEntryById(@PathVariable Long myId) {
+        return journalEntries.remove(myId);
+    }
+
+    @PutMapping("id/{myId}")
+    public JournalEntry updateJouranalEntryById(@PathVariable Long myId, @RequestBody JournalEntry myEntry) {
+        return journalEntries.put(myId, myEntry);
+    }
+
+    
 }
