dfd898a58c034611be067ebdb14fd129b334e446
Jivesh Kumar <jivesh.kumar@technoforte.co.in>
2024-06-27 17:53:44 +0530

    Authorised to user for get , put , post , delete

--- src/main/java/com/jivesh/journal/security/secureConfig.java
+++ src/main/java/com/jivesh/journal/security/secureConfig.java
@@ -2,9 +2,13 @@ package com.jivesh.journal.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 
 @Configuration
 public class secureConfig {
@@ -31,5 +35,23 @@ public class secureConfig {
                 .build();
         
         return new InMemoryUserDetailsManager(jivesh, lochan, rohit);
+    }
+    
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    	
+        http.authorizeRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "/crud").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/crud/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/crud").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/crud/**").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/crud/**").hasRole("ADMIN")
+        );
+    	
+        http.httpBasic(Customizer.withDefaults());
+        http.csrf(csrf -> csrf.disable());
+    	
+        return http.build();
     }
 }
