2fefb82c7bf67c4599603dc2e0fc996694358bca
Jivesh Kumar <jivesh.kumar@technoforte.co.in>
2024-06-28 17:38:32 +0530

    user id authorization vs authentication using custom table in database

--- src/main/java/com/jivesh/journal/security/secureConfig.java
+++ src/main/java/com/jivesh/journal/security/secureConfig.java
@@ -5,9 +5,6 @@ import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -17,42 +14,34 @@ import org.springframework.security.config.Customizer;
 @Configuration
 public class secureConfig {
 
-	@Bean
-	public UserDetailsManager userDetailsManager(DataSource dataSource) {
-		return new JdbcUserDetailsManager(dataSource);
-	}
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
 
-	
-	/*
-	 * @Bean public InMemoryUserDetailsManager userDetailsManager() {
-	 * 
-	 * UserDetails jivesh = User.builder() .username("jivesh")
-	 * .password("{noop}kumar") .roles("EMPLOYEE", "MANAGER", "ADMIN") .build();
-	 * 
-	 * UserDetails lochan = User.builder() .username("lochan")
-	 * .password("{noop}kumar") .roles("EMPLOYEE", "MANAGER") .build();
-	 * 
-	 * UserDetails rohit = User.builder() .username("rohit")
-	 * .password("{noop}kumar") .roles("EMPLOYEE") .build();
-	 * 
-	 * return new InMemoryUserDetailsManager(jivesh, lochan, rohit); }
-	 */
-    
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+            "SELECT user_id, pw, enabled FROM members WHERE user_id = ?");
+
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+            "SELECT user_id, role FROM roles WHERE user_id = ?");
+
+        return jdbcUserDetailsManager;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    	
         http.authorizeRequests(configurer ->
-                configurer
-                        .requestMatchers(HttpMethod.GET, "/crud").hasRole("EMPLOYEE")
-                        .requestMatchers(HttpMethod.GET, "/crud/**").hasRole("EMPLOYEE")
-                        .requestMatchers(HttpMethod.POST, "/crud").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.PUT, "/crud/**").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.DELETE, "/crud/**").hasRole("ADMIN")
+            configurer
+                .requestMatchers(HttpMethod.GET, "/crud").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.GET, "/crud/**").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.POST, "/crud").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.PUT, "/crud/**").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.DELETE, "/crud/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
         );
-    	
+
         http.httpBasic(Customizer.withDefaults());
         http.csrf(csrf -> csrf.disable());
-    	
+
         return http.build();
     }
 }
