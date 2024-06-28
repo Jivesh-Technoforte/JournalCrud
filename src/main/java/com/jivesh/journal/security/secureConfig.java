3add10b6a70d0e4ff3a097664971a81e174c176f
Jivesh Kumar <jivesh.kumar@technoforte.co.in>
2024-06-28 16:09:30 +0530

    User id and password stored in database with its role

--- src/main/java/com/jivesh/journal/security/secureConfig.java
+++ src/main/java/com/jivesh/journal/security/secureConfig.java
@@ -1,11 +1,15 @@
 package com.jivesh.journal.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
@@ -13,29 +17,26 @@ import org.springframework.security.config.Customizer;
 @Configuration
 public class secureConfig {
 
-    @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-        
-        UserDetails jivesh = User.builder()
-                .username("jivesh")
-                .password("{noop}kumar")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
-        
-        UserDetails lochan = User.builder()
-                .username("lochan")
-                .password("{noop}kumar")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
-        
-        UserDetails rohit = User.builder()
-                .username("rohit")
-                .password("{noop}kumar")
-                .roles("EMPLOYEE")
-                .build();
-        
-        return new InMemoryUserDetailsManager(jivesh, lochan, rohit);
-    }
+	@Bean
+	public UserDetailsManager userDetailsManager(DataSource dataSource) {
+		return new JdbcUserDetailsManager(dataSource);
+	}
+
+	
+	/*
+	 * @Bean public InMemoryUserDetailsManager userDetailsManager() {
+	 * 
+	 * UserDetails jivesh = User.builder() .username("jivesh")
+	 * .password("{noop}kumar") .roles("EMPLOYEE", "MANAGER", "ADMIN") .build();
+	 * 
+	 * UserDetails lochan = User.builder() .username("lochan")
+	 * .password("{noop}kumar") .roles("EMPLOYEE", "MANAGER") .build();
+	 * 
+	 * UserDetails rohit = User.builder() .username("rohit")
+	 * .password("{noop}kumar") .roles("EMPLOYEE") .build();
+	 * 
+	 * return new InMemoryUserDetailsManager(jivesh, lochan, rohit); }
+	 */
     
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
